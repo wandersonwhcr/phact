295794122f9e12b6ccd8abd7eae2bcd0aba32480
Wanderson <wandersonwhcr@gmail.com>
2014-09-25 11:55:30 -0300

    Initial Phalcon Events Manager configuration.

--- src/Manager.php
+++ src/Manager.php
@@ -1,16 +1,47 @@
 <?php
 namespace Phact;
 
+use Phalcon\Events\EventsAwareInterface;
+use Phalcon\Events\Manager as EventsManager;
+
 /**
  * Phact Event Manager
  */
-class Manager
+class Manager implements EventsAwareInterface
 {
+    /**
+     * Events Manager
+     * @var EventsManager
+     */
+    private $eventsManager;
+
     /**
      * Nodes
      * @var NodeInterface[]
      */
     private $nodes = array();
+
+    /**
+     * Set Events Manager
+     *
+     * @param  EventsManager $eventsManager Element
+     * @return Manager       Fluent Interface
+     */
+    public function setEventsManager($eventsManager)
+    {
+        $this->eventsManager = $eventsManager;
+        return $this;
+    }
+
+    /**
+     * Get Events Manager
+     *
+     * @return EventsManager Element
+     */
+    public function getEventsManager()
+    {
+        return $this->eventsManager;
+    }
 
     /**
      * Add a Node
--- tests/ManagerTest.php
+++ tests/ManagerTest.php
@@ -3,17 +3,27 @@ namespace PhactTest;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use Phact\Manager;
+use Phalcon\Events\Manager as EventsManager;
 
 class ManagerTest extends TestCase
 {
     protected function buildManager()
     {
-        return new Manager();
+        $manager = (new Manager())
+            ->setEventsManager(new EventsManager());
+        return $manager;
     }
 
     protected function buildNode()
     {
         return $this->getMock('Phact\NodeInterface');
+    }
+
+    public function testConstructor()
+    {
+        $manager = $this->buildManager();
+
+        $this->assertInstanceOf('Phalcon\Events\EventsAwareInterface', $manager);
     }
 
     public function testAdd()
